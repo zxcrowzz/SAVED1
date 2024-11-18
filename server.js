6b3319247f55ca51053c96bf0eb12852f76c1eab
zxcrowzz <pantsbro4@gmail.com>
2024-11-18 07:18:25 -0800

    Update server.js

--- server.js
+++ server.js
@@ -281,7 +281,7 @@ function initialize(passport) {
 initialize(passport);
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://kingcod163:Saggytits101@cluster0.rcyom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://pantsbro4:Saggytits101@cluster0.mthcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     serverSelectionTimeoutMS: 30000
     
 })
